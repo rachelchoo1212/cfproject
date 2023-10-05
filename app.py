36b09165ecd07048cc637a914cf6bc1c77a24f55
rachelchoo1212 <56731819+rachelchoo1212@users.noreply.github.com>
2023-10-05 10:38:09 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+@app.route('/api/testendpoint', methods=['GET'])
+def get_endpoint():
+    return str(request.headers)
+
+@app.route('/api/limitedendpoint', methods=['GET'])
+def get_limited():
+    return "This page is for limited users"
+
+@app.route('/')
+def hello():
+    return 'Welcome to My Watchlist!'
+
+if __name__ == '__main__':
+    # 运行应用程序并监听指定的主机和端口
+    app.run(debug=True, host='0.0.0.0', port = 443, ssl_context=('/home/ec2-user/cftest/www.rachel-zhu.com.pem','/home/ec2-user/cftest/www.rachel-zhu.com.key'))
